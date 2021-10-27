c5e2ac27071860a439f8d15da93710a0b9bafae8
Heber Leonard <heber.leonard2@gmail.com>
2021-10-27 09:07:24 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,10 +6,10 @@ import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   // Switch -> Exibir apenas uma rota
-  <Switch>
+
     <Route path="/" exact component={Dashboard} />
     <Route path="/repositories/:repository+" component={Repository} />
-  </Switch>
+
 );
 
 export default Routes;
