40e4f646dc5a5dd1cf8eb7c58618feaff4ddcb3b
Heber Leonard <heber.leonard2@gmail.com>
2021-10-27 09:55:06 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   // Switch -> Exibir apenas uma rota
-  <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/repositories/:repository+" component={Repository} />
-  </Switch>
-
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repositories/:repository+" component={Repository} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
